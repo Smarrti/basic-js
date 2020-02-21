1e82230a4f7968da9c636393fa64e6e0cbda1811
Sergey <bashkin2013@mail.ru>
2020-02-21 23:33:47 +0400

    feat: add function for what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,36 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if ((date.hasOwnProperty('getMonth'))) {
+    throw Error;
+  };
+  switch (date.getMonth()) {
+    case 0:
+      return 'winter';
+    case 1:
+      return 'winter';
+    case 2:
+      return 'spring';
+    case 3:
+      return 'spring';
+    case 4:
+      return 'spring';
+    case 5:
+      return 'summer';
+    case 6:
+      return 'summer';
+    case 7:
+      return 'summer';
+    case 8:
+      return 'autumn';
+    case 9:
+      return 'autumn';
+    case 10:
+      return 'autumn';
+    case 11:
+      return 'winter';
+    default:
+      return false;
+  }
 };
