df1dc5e9f734af7d3ac60e0a0228817c2e448436
Sergey <bashkin2013@mail.ru>
2020-02-22 20:50:59 +0400

    fix: fix function transform-array. all test complete

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,37 +2,29 @@ module.exports = function transform(arr) {
     if (!Array.isArray(arr)) {
         throw Error;
     }
-    let del = new Array();
-    arr.map((el, i) => {
-        switch (el) {
+    let res = new Array();
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                res.pop();
+                break;
             case '--double-next':
-                if (arr[i + 1]) {
-                    arr[i] = arr[i + 1];
+                if ((arr[i + 1]) || ((isNaN(arr[i + 1])) && (i !== arr.length - 1)) || (arr[i + 1] == false)) {
+                    res.push(arr[i + 1]);
                 }
                 break;
             case '--double-prev':
-                if (arr[i - 1]) {
-                    arr[i] = arr[i - 1];
+                if ((arr[i - 1]) || ((isNaN(arr[i - 1])) && (i !== 0)) || (arr[i - 1] == false)) {
+                    res.push(arr[i - 1]);
                 }
                 break;
-            case '--discard-next':
-                    del.push(i);
+            default:
+                res.push(arr[i]);
                 break;
-            case '--discard-prev':
-                if (arr[i - 1]) {
-                    del.push(i - 1);
-                } else {
-                    del.push(i);
-                }
-        }
-    });
-    for (let i = del.length - 1; i >= 0; i--) {
-        let el = del[i];
-        if (arr[el + 1]) {
-            arr.splice(el, 2);
-        } else {
-            arr.splice(el, 1);
         }
     }
-    return arr;
+    return res;
 };
