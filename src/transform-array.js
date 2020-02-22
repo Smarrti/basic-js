0d115cbd1956e6577180c8127cfaa565bdff4e1f
Smarrti <bashkin2013@mail.ru>
2020-02-22 11:19:52 +0400

    feat: add function for transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,38 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw Error;
+    }
+    let del = new Array();
+    arr.map((el, i) => {
+        switch (el) {
+            case '--double-next':
+                if (arr[i + 1]) {
+                    arr[i] = arr[i + 1];
+                }
+                break;
+            case '--double-prev':
+                if (arr[i - 1]) {
+                    arr[i] = arr[i - 1];
+                }
+                break;
+            case '--discard-next':
+                    del.push(i);
+                break;
+            case '--discard-prev':
+                if (arr[i - 1]) {
+                    del.push(i - 1);
+                } else {
+                    del.push(i);
+                }
+        }
+    });
+    for (let i = del.length - 1; i >= 0; i--) {
+        let el = del[i];
+        if (arr[el + 1]) {
+            arr.splice(el, 2);
+        } else {
+            arr.splice(el, 1);
+        }
+    }
+    return arr;
 };
